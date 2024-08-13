dc738159c890eb0f7c16734ad19c4c244479db4b
amellyaa12 <lyaaamel4@gmail.com>
2024-08-13 16:07:58 +0700

    first commit

---
+++ luas jajargenjang.php
@@ -0,0 +1,16 @@
+<?php
+//Mulai
+//Deklarasi , alas (a) dan tinggi (t)
+$alas =10;
+$tinggi= 5;
+$luas=alas*tinggi
+//Dari jajargenjang
+echo "alas". $alas;
+echo "tinggi". $tinggi;
+echo "luas". $alas
+//Hitung luas dengan rumus
+$luas=$alas*$tinggi*$luas;
+//Luas=alas*tinggi
+echo "<berhasil luas jajargenjang=", $luas
+//Selesai
+
